70f70ee1683cb085aed47a32ee9ceaf5a28c6e0c
Thiago Teofilo <thiago.teophilos@gmail.com>
2024-10-01 21:05:55 -0300

    fix(projects): corrigir responsividade dos projetos

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -6,10 +6,14 @@
     <div class="section grid gap-4 my-5 justify-items-center">
       <div v-for="(project, i) in PROJECTS" :key="i" class="project-card">
         <a :href="project.url" target="_blank">
-          <div :class="['images p-4 rounded', project.imageWrapperClasses]">
+          <div
+            :class="[
+              'images p-4 pl-1 pr-5 rounded-xl',
+              project.imageWrapperClasses,
+            ]"
+          >
             <img :src="getImageUrl(project.image)" alt="" />
             <img
-              v-if="project.mobileImage"
               :src="getImageUrl(project.mobileImage)"
               :class="[project.imageClasses]"
               alt=""
@@ -35,15 +39,11 @@ function getImageUrl(imageName: string) {
 
 <style lang="postcss" scoped>
 .project-card {
-  @apply w-[calc(50%-1rem)] transition-colors border border-solid border-lightSecondary dark:border-secondary p-4 rounded-lg bg-lightSecondary dark:bg-secondary;
+  @apply w-full sm:w-[calc(50%-1rem)] 2xl:w-[calc(33.3%-1rem)] transition-colors border border-solid border-lightSecondary dark:border-secondary p-4 rounded-xl bg-lightSecondary dark:bg-secondary;
 }
 
 .project-card:hover {
   @apply border-lightSecondaryAlt dark:border-secondary-alt;
-}
-
-.project-card img {
-  @apply rounded;
 }
 
 .project-card .images {
@@ -51,7 +51,7 @@ function getImageUrl(imageName: string) {
 }
 
 .project-card img:last-child {
-  @apply absolute w-20 bottom-1 right-1 border-4 border-solid;
+  @apply rounded-md absolute w-[23%] -bottom-0 right-1 border-4 border-solid;
 }
 
 .project-card h4 {
@@ -59,6 +59,6 @@ function getImageUrl(imageName: string) {
 }
 
 .project-card p {
-  @apply mt-0;
+  @apply mt-0 text-left;
 }
 </style>
