e0298887fe84a6fd6777ac7ef5994d4a67f689f3
Thiago Teofilo <thiago.teophilos@gmail.com>
2024-10-03 12:39:03 -0300

    fix(vue.config): corrigir configuração do vue

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
-const webpack = require('webpack');
-
 module.exports = defineConfig({
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        // Set feature flags based on your requirements
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false', // Disable hydration mismatch details
-        // Add other feature flags if needed
-      }),
-    ],
-  },
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 });
